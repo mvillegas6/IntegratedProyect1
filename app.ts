d7b1c07891ccb76220c2912f7d2f5ed8f44dd18e
Esteban Muriel <emurielr@eafit.edu.co>
2023-08-10 17:03:04 -0500

    ejs

--- app.ts
+++ app.ts
@@ -1,11 +1,14 @@
 import express from 'express';
-
+const path = require('path');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-    res.send('Application works!');
-  });
+  res.render('index', { hola: 'hola' });
+});
 
 app.listen(3000, () => {
-    console.log('Application started on port 3000!');
-  });
+  console.log('Application started on port 3000!');
+});

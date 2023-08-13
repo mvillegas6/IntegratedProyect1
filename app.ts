0f6ee2a91a69d3f0e689fc41630325237a4936aa
Manuel Villegas <manuel.villegas.michel@gmail.com>
2023-08-13 11:23:39 -0500

    dotenv module and .env DB_URL

--- app.ts
+++ app.ts
@@ -10,6 +10,7 @@ import methodOverride from 'method-override';
 const app = express();
 
 dbconection();
+// seedData();
 
 app.engine('ejs', ejsMate);
 app.use(express.urlencoded({ extended: true }));
@@ -22,9 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/posts', postRouter);
 
 app.get('/', (req: Request, res: Response) => {
-  res.render('index');
+    res.render('index');
 });
 
 app.listen(3000, () => {
-  console.log('Application started on port 3000!');
+    console.log('Application started on port 3000!');
 });
--- database.ts
+++ database.ts
@@ -1,14 +1,17 @@
 import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const dbconection = () => {
-  mongoose
-    .connect('mongodb://127.0.0.1:27017/Tutorly')
-    .then(() => {
-      console.log('CONNECTED');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    mongoose
+        .connect(process.env.DB_URL!)
+        .then(() => {
+            console.log('CONNECTED');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 export = dbconection;

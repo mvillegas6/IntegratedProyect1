3d68c348d025c0d9d2c582771cbf0b7ecfd4ef8e
Manuel Villegas <manuel.villegas.michel@gmail.com>
2023-08-27 16:25:46 -0500

    Main page of each posts start styling

--- app.ts
+++ app.ts
@@ -11,10 +11,8 @@ import session from 'express-session';
 import cookieParser from 'cookie-parser';
 import flash from 'connect-flash';
 import passport from 'passport';
-import {Strategy as localStrategy} from 'passport-local';
-import {User} from './models/user';
-
-
+import { Strategy as localStrategy } from 'passport-local';
+import { User } from './models/user';
 
 const app = express();
 
@@ -29,13 +27,18 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser('alo'));
-app.use(session({
-  name: 'YSC__',
-  secret: 'Dirty deeds done dirty cheap',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: Date.now() + 1000 * 60 * 60 * 24 * 7, httpOnly: true }
-}));
+app.use(
+    session({
+        name: 'YSC__',
+        secret: 'Dirty deeds done dirty cheap',
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            maxAge: Date.now() + 1000 * 60 * 60 * 24 * 7,
+            httpOnly: true,
+        },
+    })
+);
 app.use(session());
 app.use(flash());
 app.use(passport.initialize());
@@ -45,17 +48,15 @@ passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use((req:Request, res: Response, next: NextFunction) => {
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error');
-  res.locals.currentUser = req.user;
-  next();
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    res.locals.currentUser = req.user;
+    next();
 });
-
 
 app.use('/posts', postRouter);
 app.use('', userRouter);
-
 
 app.get('/', (req: Request, res: Response) => {
     res.render('index');
